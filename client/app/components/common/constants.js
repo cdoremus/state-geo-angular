8a7290e7b4e8df7a2c7b3b142af1f89280a597e1
Craig Doremus <cdoremus@gmail.com>
2015-09-12 20:09:38 -0400

    Added ObjectNotArray message

--- client/app/components/common/constants.js
+++ client/app/components/common/constants.js
@@ -1,4 +1,5 @@
 
 export const messages = {
-	CandidateAdjacentsNotPicked : "Please select at least one adjacent state"
+	CandidateAdjacentsNotPicked : "Please select at least one adjacent state",
+	ObjectNotAnArray: "Object is not an array: "
 };
