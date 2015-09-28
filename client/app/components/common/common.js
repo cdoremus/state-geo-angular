d036ee4f52890db1dac34a913c2893a4ef690b88
Craig Doremus <cdoremus@gmail.com>
2015-09-28 16:43:56 -0400

    Remove unused empty file

--- client/app/components/common/common.js
+++
@@ -1,7 +0,0 @@
-import angular from 'angular';
-/*
-import {textboxDirective} from './textbox/textbox.directive';
-
-export const common = angular.module('common', [])
-  .directive('textbox', textboxDirective);
-*/
