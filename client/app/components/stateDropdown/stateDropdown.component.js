71a2f4d0bbf1f97c95431114f8ba4326d4b04423
Craig Doremus <cdoremus@gmail.com>
2015-10-12 13:41:48 -0400

    Component now works and is used by the adjacentQuiz component

--- client/app/components/stateDropdown/stateDropdown.component.js
+++ client/app/components/stateDropdown/stateDropdown.component.js
@@ -1,6 +1,7 @@
 import './stateDropdown.styl';
 import {StateDropdownComponent as controller} from './stateDropdown.component';
-import {StateDropdownService as stateDropdownService} from './stateDropdown.service';
+import {StateService as stateService} from '../common/state.service';
+import * as util from '../common/utilities';
 import template from './stateDropdown.html';
 
 export const stateDropdownDirective = ()=> {
@@ -8,32 +9,57 @@ export const stateDropdownDirective = ()=> {
     controller,
     template,
     controllerAs: 'vm',
-    scope: {},
+    scope: {
+      componentId: '@',
+      componentLabel: '@',
+      clickListener: '&',
+      selectedStateListener: '&'
+    },
     replace: true,
-    restrict: 'E',
     bindToController: true,    
-    transclude: true
+    restrict: 'E'
   }
 };
 
 class StateDropdownComponent {
-  constructor(stateDropdownService) {
-    this.greeting = 'StateDropdownComponent!';
-    this.service = stateDropdownService;
-    this.selectedState = {};
+  
+  constructor(stateService) {
+    this.service = stateService;
+    this.populatePageData();
     this.states = [];
+    this.selectedState = {};
+    console.log("StateDropdownComponent: " + this.toString());
   }
 
-  populateDropdown() {
-    
+  /* Fill adjacentStates array and pick a random one to display in drop down */
+  populatePageData() {
+  	this.service.queryAdjacentStates()
+		.then(result => {
+      this.states = util.sortArrayByProperty(result.data, 'name');
+      })
+    .then(result => {
+      //select a random state from array
+      this.selectedState = util.randomArrayItem(this.states);
+      this.selectedStateListener({state: this.selectedState});
+      console.log("Selected state in StateDropdownComponent: ", this.selectedState);
+    });
   }
-
-  clickHandler() {
-    
+  
+  onClick() {
+      this.selectedStateListener({state: this.selectedState});
+      this.clickListener({state: this.selectedState});
   }
-
+  
+  toString() {
+    return `
+      componentId: ${this.componentId},
+      componentLabel: ${this.componentLabel},
+      selectedState: ${this.selectedState},
+      states: ${this.states}
+      `    
+  }
 }
 
-StateDropdownComponent.$inject = ['stateDropdownService'];
+StateDropdownComponent.$inject = ['stateService'];
 
 export {StateDropdownComponent};
