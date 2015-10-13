080c3ed5a0554360047e30b540776bb7f32f565b
Craig Doremus <cdoremus@gmail.com>
2015-10-12 23:32:44 -0400

    Fixed broken tests

--- client/app/components/stateDropdown/stateDropdown.spec.js
+++ client/app/components/stateDropdown/stateDropdown.spec.js
@@ -4,11 +4,12 @@ import template from './stateDropdown.html';
 
 describe('StateDropdown', ()=>{
   let $rootScope,
-  makeController;
+  let q;
 
   beforeEach(window.module(stateDropdown.name));
-  beforeEach(inject((_$rootScope_)=>{
+  beforeEach(inject((_$rootScope_, $q)=>{
     $rootScope = _$rootScope_;
+    q = $q;
   }));
 
   describe('Module', ()=>{
@@ -21,17 +22,17 @@ describe('StateDropdown', ()=>{
   describe('Controller', ()=>{
     // test your controller here
 
-    it('should have a name property [REMOVE]', ()=>{ // erase me if you remove this.name from the controller
-      let controller = new StateDropdownComponent();
+    it('should have a states property', ()=>{ 
+      let stateService =  new MockStateService(q);
+      let controller = new StateDropdownComponent(stateService);
 
-      expect(controller).to.have.property('greeting');
+      expect(controller).to.have.property('states');
     });
   });
 
   describe('Template', ()=>{
     // test the template
     // use Regexes to test that you are using the right bindings {{  }}
-
     it('should have name in template [REMOVE]', ()=>{
       expect(template).to.match(/{{\s?vm\.greeting\s?}}/g);
     });
@@ -55,3 +56,16 @@ describe('StateDropdown', ()=>{
       });
   });
 });
+
+class MockStateService {
+    constructor($q) {
+      this.q = $q;
+    }
+    querystates() {
+      let deferred = this.q.defer();
+      return deferred.promise;
+    }
+    populateAdjacentStates() {
+      return [];
+    }
+}; 
