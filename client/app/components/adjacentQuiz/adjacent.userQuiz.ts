11ef3fd5078add33db68f24529f0272885162560
Craig Doremus <cdoremus@gmail.com>
2016-04-03 20:39:25 -0400

    Exposed properties to fix typescript error

--- client/app/components/adjacentQuiz/adjacent.userQuiz.ts
+++ client/app/components/adjacentQuiz/adjacent.userQuiz.ts
@@ -1,9 +1,13 @@
 
 /* A domain object to hold quiz results for a user */
 export default class UserQuizResults {
-	constructor() {
-		this.username;
-		this.totalAnswers;
-		this.correctAnswers;
-	}
+  username: string;
+  totalAnswers: any[];
+  correctAnswers: any[];
+
+  constructor() {
+    this.username;
+    this.totalAnswers;
+    this.correctAnswers;
+  }
 }
