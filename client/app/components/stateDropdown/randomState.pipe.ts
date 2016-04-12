64693c1befa866bec63c7f3b1fa26bf850e63ed3
Craig Doremus <cdoremus@gmail.com>
2016-04-12 14:26:40 -0400

    Tried out transform() returning an immutable changed array with only random item modified. It did not work; with default change detention strategy, it causes an endless loop.

--- client/app/components/stateDropdown/randomState.pipe.ts
+++ client/app/components/stateDropdown/randomState.pipe.ts
@@ -2,10 +2,12 @@ import {Pipe, PipeTransform, ChangeDetectorRef, Inject} from 'angular2/core';
 import {State} from '../common/state';
 
 /**
- * Angular 2 pipe that selects a random State fron an array of State objects.
+ * Angular 2 pipe that selects a random State fron an array of State objects
+ * and changes its selected property to 'selected'.
  */
-@Pipe({name: 'randomState'})
+@Pipe({name: 'randomState', pure: false})
 export default class RandomStatePipe implements PipeTransform {
+  // private selectedStates: State[] = [];
   private changeDetectorRef: ChangeDetectorRef;
 
   constructor(@Inject(ChangeDetectorRef) cdRef:ChangeDetectorRef) {
@@ -14,24 +16,27 @@ export default class RandomStatePipe implements PipeTransform {
 
   transform(input: State[], args: string[]) {
     if (input && input != null && input.length > 0) {
+
       let len = input.length;
       let rand = Math.floor(Math.random() * len);
       let count = 0;
-      let state = '';
       input.forEach(state => {
         if (count === rand) {
-          state.selected = "";
-          state = staten.name;
-        // } else {
-        //   state.selected = "false";
+          state.selected = 'selected';
         }
         count++;
       });
-      console.log("RandomStatePipe input.length " + input.length + " Random stat index: " + rand);
+      console.log("RandomStatePipe random state: " + input[rand].name + ' with index ' + rand);
+      // this.selectedStates.push(...input);
+
+      // patch together a new array wfrom the old one with the random one having the selected property set
+      // let newInput = [...input.slice(0, rand), Object.assign({}, input[rand], {selected: ""}), ...input.slice(rand + 1)];
+      // console.log("New array from RandomStatePipe with selected index " + rand, newInput);
+      // this.changeDetectorRef.markForCheck();
+      // return newInput;
+
       this.changeDetectorRef.markForCheck();
       return input;
-    // } else {
-    //    return [];
     }
   }
 }
