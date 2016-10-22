c7a888795a08ef8c7e738870073456bccacbebf3
Craig Doremus <cdoremus@gmail.com>
2016-10-21 23:21:05 -0400

    Removed leftover from NG1 app

--- client/app/shared/shared.js
+++
@@ -1,8 +0,0 @@
-import {api} from './api';
-import {posts} from './posts';
-import angular from 'angular';
-
-export const shared = angular.module('shared', [])
-  .constant('API', api)
-  .factory('Posts', posts);
-
