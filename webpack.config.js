a0b543acd756ab70a13e17048bf923443a266da9
Craig Doremus <cdoremus@gmail.com>
2016-10-22 21:42:02 -0400

    Added validator and plugins. devServer needs work.

--- webpack.config.js
+++ webpack.config.js
@@ -6,23 +6,49 @@ but we use gulp to orchestrate
  */
 var webpack = require('webpack');
 var path = require('path');
+var validate = require('webpack-validator')
+// Webpack Plugins
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+// var autoprefixer = require('autoprefixer'); //NOT USED
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+// var DashboardPlugin = require('webpack-dashboard/plugin'); //NOT USED
 
+/**
+ *
+ * @param Relative directory or file path.
+ * When called with multiple arguments, they get concatinated
+ * together and then appended to the full path.
+ * return Full path to file or directory using args or the
+ * full path to this diectory if called with no args.
+ */
 function root(args) {
   args = Array.prototype.slice.call(arguments, 0);
+  // Note: __dirname is the full path to the current directory
   return path.join.apply(path, [__dirname].concat(args));
 }
 
-module.exports = {
+var config = {
+
+  entry: root('client/app/main.js'),
+
   output: {
+    path: root('dist'),
     filename: 'bundle.js'
   },
 
-  devtool: 'sourcemap',
-
-  entry: './client/app/boot.js',
+  devtool: 'source-map',
 
   resolve: {
-    extensions: ['', '.js']
+    // cache: true,
+    // root: root(),
+    // only discover files that have those extensions
+    //extensions: ['', '.js'],
+    extensions: ['.ts', '.js', '.json', '.css', '.scss', '.html'],
+    alias: {
+      'app': 'client/app'
+    }
   },
 
   module: {
@@ -31,15 +57,68 @@ module.exports = {
 //     ],
     loaders: [
       { test: /\.html$/, loader: 'raw' },
-      { test: /\.json$/, loader: 'file' },
-      { test: /\.styl$/, loader: 'style!css!stylus' },
+      { test: /\.json$/, loader: 'json' },
+      // { test: /\.styl$/, loader: 'style!css!stylus' },
       { test: /\.css/, loader: 'style!css' },
-      { test: /\.js$/, loader: 'babel?stage=1', exclude: [/client\/lib/, /node_modules/, /\.spec\.js/] }
-    //   { test: /\.ts$/, loader: 'awesome-typescript-loader', exclude: [ /\.(spec|e2e)\.ts$/ ] }
+      // { test: /\.js$/, loader: 'babel?stage=1', exclude: [/client\/lib/, /node_modules/, /\.spec\.js/] },
+      { test: /\.ts$/, loaders: ['awesome-typescript-loader', 'angular2-template-loader', '@angularclass/hmr-loader'], exclude: [ /\.(spec|e2e)\.ts$/ ] }
      ]
   },
 
-  stylus: {
-    use: [require('jeet')(), require('rupture')(), require('bootstrap-styl')()]
-  }
+  devServer: {
+      contentBase: root('dist'),
+      historyApiFallback: true,
+      quiet: true,
+      stats: 'minimal' // none (or false), errors-only, minimal, normal (or true) and verbose
+  },
+
+  // stylus: {
+  //   use: [require('jeet')(), require('rupture')(), require('bootstrap-styl')()]
+  // },
+
+  plugins: [
+     // Inject script and link tags into html files
+      // Reference: https://github.com/ampedandwired/html-webpack-plugin
+      new HtmlWebpackPlugin({
+        template: './client/index.html',
+        chunksSortMode: 'dependency'
+      }),
+
+     // Generate common chunks if necessary
+      // Reference: https://webpack.github.io/docs/code-splitting.html
+      // Reference: https://webpack.github.io/docs/list-of-plugins.html#commonschunkplugin
+      // new CommonsChunkPlugin({
+      //   name: ['vendor', 'polyfills']
+      // }),
+
+      // Extract css files
+      // Reference: https://github.com/webpack/extract-text-webpack-plugin
+      // Disabled when in test mode or not in build mode
+      // new ExtractTextPlugin('css/[name].css'),
+
+      // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
+      // Only emit files when there are no errors
+      new webpack.NoErrorsPlugin(),
+
+      // Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
+      // Dedupe modules in the output
+      // new webpack.optimize.DedupePlugin(),
+
+      // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+      // Minify all javascript, switch loaders to minimizing mode
+//      new webpack.optimize.UglifyJsPlugin({mangle: { keep_fnames: true }}),
+
+      // Copy assets from the public folder
+      // Reference: https://github.com/kevlened/copy-webpack-plugin
+      new CopyWebpackPlugin([
+        { from: 'client/index.css' },
+        { from: 'client/adjacentStates.json' },
+        { from: 'client/states.json' },
+        { from: 'client/users.json' }
+      ])
+
+
+  ]
 };
+
+module.exports = validate(config);
