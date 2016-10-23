461805dc28f3f9fadb9db19b3b6055d9362d2f80
Craig Doremus <cdoremus@gmail.com>
2016-10-22 21:38:52 -0400

    Removed zone and reflect-metadata imports

--- client/app/main.ts
+++ client/app/main.ts
@@ -1,5 +1,5 @@
-import 'zone.js';
-import 'reflect-metadata';
+// import 'zone.js';
+// import 'reflect-metadata';
 
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
